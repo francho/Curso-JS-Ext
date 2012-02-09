541338052ab822878dcf1d4e3abbd637ba1be79f
Francho <francho@centraldereservas.com>
2012-02-09 13:18:08 +0100

    maqueo de código

--- src/dia3/Panel.js
+++ src/dia3/Panel.js
@@ -6,35 +6,37 @@
  * To change this template use File | Settings | File Templates.
  */
 
-App.define('Panel',{
-    init: function(options){
+App.define('Panel', {
+    init:function (options) {
         // Establecemos las propiedades que nos pasan en la llamada al constructor
         App.copy(this, options || {});
 
         // Buscamos el destino
-        var padre = Ext.DomQuery.select('#'+this.renderTo);
+        var padre = Ext.DomQuery.select('#' + this.renderTo);
 
-        for(var i=0, size=padre.length; i < size; i++) {
+        for (var i = 0, size = padre.length; i < size; i++) {
             // Le añadimos el panel
             var newPanel = Ext.DomHelper.append(padre[i], {
-                tag : 'div',
-                cls: 'panel',
-                children: [{
-                    tag: 'h1',
-                    html: this.title,
-                    cls: 'panel-title'
-
-                },{
-                    tag: 'p',
-                    html: this.content,
-                    cls: 'panel-content'
-                }]
+                tag:'div',
+                cls:'panel',
+                children:[
+                    {
+                        tag:'h1',
+                        html:this.title,
+                        cls:'panel-title'
+                    },
+                    {
+                        tag:'p',
+                        html:this.content,
+                        cls:'panel-content'
+                    }
+                ]
 
             });
 
             // Modificamos la altura y la altura del objeto creado
-            Ext.fly(newPanel).setWidth(this.width,true).setHeight(this.height,true);
+            Ext.fly(newPanel).setWidth(this.width, true).setHeight(this.height, true);
         }
 
-        }
+    }
 });
